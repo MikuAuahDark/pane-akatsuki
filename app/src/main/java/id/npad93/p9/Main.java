fb86129e6d02bf8e655bbbdad85f5bdfdbb9ad36
Miku AuahDark <lumi.hotaru@gmail.com>
2023-05-19 00:01:55 +0800

    Init

---
+++ app/src/main/java/id/npad93/p9/Main.java
@@ -0,0 +1,9 @@
+package id.npad93.p9;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Launching");
+        App.main(args);
+        System.out.println("Ok done!");
+    }
+}
